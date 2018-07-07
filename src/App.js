3f2ef913f04915f9d26ef0255fcadbae7ade9637
SamuelLJackson <severett29@gmail.com>
2018-07-07 12:05:43 -0400

    first commit for front-end branch

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ class App extends Component {
         simpleStorageInstance = instance
 
         // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(5, {from: accounts[0]})
+        return simpleStorageInstance.set(100, {from: accounts[0]})
       }).then((result) => {
         // Get the value from the contract to prove it worked.
         return simpleStorageInstance.get.call(accounts[0])
@@ -71,22 +71,22 @@ class App extends Component {
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
-            <a href="#" className="pure-menu-heading pure-menu-link">Truffle Box</a>
+            <a href="#" className="pure-menu-heading pure-menu-link">Token Bones</a>
         </nav>
-
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-              <h1>Good to Go!</h1>
-              <p>Your Truffle Box is installed and ready.</p>
-              <h2>Smart Contract Example</h2>
-              <p>If your contracts compiled and migrated successfully, below will show a stored value of 5 (by default).</p>
-              <p>Try changing the value stored on <strong>line 59</strong> of App.js.</p>
+              <h1>A minable proof of work proof of work token for oracles</h1>
+              <h3>What is it?</h3>
+              <h3>Ether to USD value</h3>
+              <h3>How does it work?</h3>
+              <h3>Download a miner!</h3>
               <p>The stored value is: {this.state.storageValue}</p>
             </div>
           </div>
         </main>
       </div>
+
     );
   }
 }

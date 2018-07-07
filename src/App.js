263558fcf055e318985d28924a433b942d725d9d
SamuelLJackson <severett29@gmail.com>
2018-07-07 19:38:58 -0400

    first contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
-import SimpleStorageContract from '../build/contracts/SimpleStorage.json'
-
+import OracleTokenContract from '../build/contracts/OracleToken.json';
 import getWeb3 from './utils/getWeb3'
 import Chart from 'chart.js';
 var LineChart = require('react-chartjs').Line;
@@ -49,25 +48,20 @@ class App extends Component {
      */
 
     const contract = require('truffle-contract')
-    const simpleStorage = contract(SimpleStorageContract)
-    simpleStorage.setProvider(this.state.web3.currentProvider)
+    const oracleToken = contract(OracleTokenContract)
+    oracleToken.setProvider(this.state.web3.currentProvider)
 
     // Declaring this for later so we can chain functions on SimpleStorage.
-    var simpleStorageInstance
+    var oracleTokenInstance
 
     // Get accounts.
-    this.state.web3.eth.getAccounts((error, accounts) => {
-      simpleStorage.deployed().then((instance) => {
-        simpleStorageInstance = instance
-        // Stores a given value, 5 by default.
-        return simpleStorageInstance.set(100, {from: accounts[0]})
-      }).then((result) => {
-        // Get the value from the contract to prove it worked.
-        return simpleStorageInstance.get.call(accounts[0])
-      }).then((result) => {
-        // Update state with the result.
-        return this.setState({ storageValue: result.c[0] })
-      })
+    this.state.web3.eth.getAccounts(async(error, accounts) => {
+      const instance = await oracleToken.at(
+        '0x2b63d6e98e66c7e9fe11225ba349b0b33234d9a2'
+      ).getVariables({from: accounts[0]})
+      .then((result) => {
+          return this.setState({challengeValue: result})
+      });
     })
   }
   componentDidMount () {
@@ -154,7 +148,7 @@ class App extends Component {
   render() {
     let imgUrl = 'https://new.consensys.net/wp-content/themes/consensys/client/images/masthead-organic-poster.jpg';
     let minerImageUrl = 'https://media.istockphoto.com/vectors/cartoon-dwarf-miner-vector-id839970312';
-
+    console.log(this.state)
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
